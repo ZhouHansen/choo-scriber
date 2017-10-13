c27d9b1380214e91396f857b940128c03de57b3b
zhouhansen <z308114274@gmail.com>
2017-10-13 16:33:10 +0800

    use sheetify-inline

--- build.js
+++ build.js
@@ -3,33 +3,35 @@ var mkdirp = require('mkdirp')
 var browserify = require('browserify')
 var html = require('bel')
 var colors = require('colors')
+var minify = require('html-minifier').minify
 
 var b = browserify()
 
 mkdirp('./build', ()=>{
   console.log('create builds folder'.green)
   b.add('index.js')
-  b.bundle((err, buf)=>{
+   .transform('sheetify',{ use: [ 'sheetify-inline', "sheetify-cssnext" ] })
+   .bundle((err, buf)=>{
     fs.writeFile('./build/bundle.js', buf, ()=>{
       console.log('create bundle.js'.green)
     })
+  })
 
-    var indexHtml = html`
-      <!doctype html>
-      <html lang="en" dir="ltr">
-      <head>
-        <title>choo-scriber</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <div></div>
-        <script src="bundle.js"></script>
-      </body>
-      </html>
-    `
+  var indexHtml = html`
+    <!doctype html>
+    <html lang="en" dir="ltr">
+    <head>
+      <title>choo-scriber</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <div></div>
+      <script src="bundle.js"></script>
+    </body>
+    </html>
+  `
 
-    fs.writeFile('./build/index.html', indexHtml, ()=>{
-      console.log('create index.html'.green)
-    })
+  fs.writeFile('./build/index.html', minify(indexHtml, {collapseWhitespace: true}), ()=>{
+    console.log('create index.html'.green)
   })
 })
