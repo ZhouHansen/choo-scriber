ee418eac61fef1c1b0df0005a064dab96873a077
zhouhansen <z308114274@gmail.com>
2017-10-23 16:04:28 +0800

    use css-extract

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var path = require('path')
 var mkdirp = require('mkdirp')
 var browserify = require('browserify')
 var html = require('bel')
@@ -6,22 +7,29 @@ var colors = require('colors')
 var minify = require('html-minifier').minify
 
 var b = browserify()
+var entry = 'index.js'
+var basedir = path.dirname(entry)
+var outdir = path.join(basedir, 'dist')
 
-mkdirp('./build', () => {
-  console.log('create builds folder'.green)
-  b.add('index.js')
+mkdirp(outdir, () => {
+  b.add(entry)
    .transform('sheetify', { use: [ 'sheetify-inline', 'sheetify-cssnext' ] })
-   .bundle((err, buf) => {
-     fs.writeFile('./build/bundle.js', buf, () => {
-       console.log('create bundle.js'.green)
-     })
-   })
+   .plugin('css-extract', { out: path.join(outdir, 'bundle.css') })
+
+  console.log('create bundle.css'.green)
+
+  b.bundle((err, buf) => {
+    fs.writeFile(path.join(outdir, 'bundle.js'), buf, () => {
+      console.log('create bundle.js'.green)
+    })
+  })
 
   var indexHtml = html`
     <!doctype html>
     <html lang="en" dir="ltr">
     <head>
       <title>choo-scriber</title>
+      <link rel="stylesheet" href="bundle.css">
       <meta charset="utf-8">
     </head>
     <body>
@@ -31,7 +39,7 @@ mkdirp('./build', () => {
     </html>
   `
 
-  fs.writeFile('./build/index.html', minify(indexHtml, {collapseWhitespace: true}), () => {
+  fs.writeFile(path.join(outdir, 'index.html'), minify(indexHtml, {collapseWhitespace: true}), () => {
     console.log('create index.html'.green)
   })
 })
