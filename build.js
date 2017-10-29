1c2ee8faca342ad0bf2c7ec12d043e69f2667b30
zhouhansen <z308114274@gmail.com>
2017-10-29 16:25:55 +0800

    rewrite build.js use buffer-graph
    and use purify-css, clean-css

--- build.js
+++ build.js
@@ -4,70 +4,76 @@ var mkdirp = require('mkdirp')
 var browserify = require('browserify')
 var html = require('bel')
 var colors = require('colors')
-var tinyify = require('tinyify')
+var purify = require('purify-css')
+var Clean = require('clean-css')
 var dedent = require('dedent')
-var exorcist = require('exorcist')
 var minify = require('html-minifier').minify
 var concat = require('concat-stream')
+var graph = require('buffer-graph')()
 
-var b = browserify({debug: true})
 var entry = 'index.js'
 var basedir = path.dirname(entry)
 var outdir = path.join(basedir, 'dist')
-var mapfile  = path.join(outdir, 'bundle.js.map')
-var concatStream = concat(gotBundle)
+var clean = new Clean(createCleanCssOptions())
 
-mkdirp(outdir, () => {
-  writeBundle()
-  writeHtml()
-  writeManifest()
-  writeServiceWorker()
+graph.on('change', (nodeName, edgeName, state) => {
+  var eventName = nodeName + ':' + edgeName
+    , file
+    , data
+    , log
+
+  if (eventName === 'scripts:bundle') {
+    file = path.join(outdir, 'bundle.js')
+    data = state.scripts.bundle.buffer
+    log = ' ✨  create bundle.js'
+  } else if (eventName === 'html:bundle') {
+    file = path.join(outdir, 'index.html')
+    data = state.html.bundle.buffer
+    log = ' ✨  create index.html'
+  } else if (eventName === 'manifest:bundle') {
+    file = path.join(outdir, 'manifest.json')
+    data = state.manifest.bundle.buffer
+    log = ' ✨  create manifest.json'
+  } else if (eventName === 'serviceWorker:bundle') {
+    file = path.join(outdir, 'sw.js')
+    data = state.serviceWorker.bundle.buffer
+    log = ' ✨  create sw.js'
+  } else if (eventName === 'styles:bundle') {
+    file = path.join(outdir, 'bundle.css')
+    data = state.styles.bundle.buffer
+    log = ' ✨  create bundle.css'
+  }
+
+  if (file) {
+    writeFile(file, data, log)
+  }
 })
 
-function gotBundle (buf) {
-  fs.writeFile(path.join(outdir, 'bundle.js'), buf, () => {
-    console.log('create bundle.js'.green)
-  })
-}
+graph.node('scripts', (state, createEdge) => {
+  var b = browserify({debug: true})
+  var mapName = 'bundle.js.map'
+  var mapFile  = path.join(outdir, mapName)
 
-function writeBundle () {
   b.add(entry)
    .transform('sheetify', { use: [ 'sheetify-inline', 'sheetify-cssnext' ] })
-   .plugin('css-extract', { out: writeCss })
-   .plugin('tinyify')
-   .bundle()
-   .pipe(exorcist(mapfile))
-   .pipe(concatStream)
-}
+   .plugin('css-extract', { out: bundleStyles })
 
-function writeCss (){
-  return concat({ encoding: 'string' }, (str) => {
-    fs.writeFile(path.join(outdir, 'bundle.css'), str, () => {
-      console.log('create bundle.css'.green)
+  bundleScripts()
+
+  function bundleStyles () {
+    return concat({ encoding: 'buffer' }, (buf) => {
+      createEdge('style', buf)
     })
-  })
-}
-
-function writeManifest () {
-  var filename = path.join(outdir, 'manifest.json')
-  var file = dedent`
-  {
-    "name": "choo-scriber",
-    "short_name": "choo-scriber",
-    "description": "A very cute app",
-    "start_url": "/",
-    "display": "standalone",
-    "background_color": "whitesmoke"
   }
-  `
 
-  fs.writeFile(filename, file, () => {
-    console.log('create manifest.json'.green)
-  })
-}
+  function bundleScripts () {
+    b.bundle((err, bundle) => {
+      createEdge('bundle', bundle)
+    })
+  }
+})
 
-function writeHtml () {
-  var filename = path.join(outdir, 'index.html')
+graph.node('html', (state, createEdge) => {
   var file = html`
     <!doctype html>
     <html lang="en" dir="ltr">
@@ -84,17 +90,55 @@ function writeHtml () {
     </html>
   `
 
-  fs.writeFile(filename, minify(file, {collapseWhitespace: true}), () => {
-    console.log('create index.html'.green)
+  createEdge('bundle', Buffer.from(String(file)))
+})
+
+graph.node('manifest', (state, createEdge) => {
+  var file = dedent`
+  {
+    "name": "choo-scriber",
+    "short_name": "choo-scriber",
+    "description": "A very cute app",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "whitesmoke"
+  }
+  `
+
+  createEdge('bundle', Buffer.from(file))
+})
+
+graph.node('serviceWorker', (state, createEdge) => {
+  fs.readFile('sw.js', (err, data) => {
+    createEdge('bundle', data)
+  })
+})
+
+graph.node('styles', [ 'scripts:style', 'scripts:bundle' ], (state, createEdge) => {
+  var script = String(state.scripts.bundle.buffer)
+    , style = String(state.scripts.style.buffer)
+    , bundle = purify(script, style, { minify: true })
+
+  bundle = clean.minify(bundle).styles
+  createEdge('bundle', Buffer.from(bundle))
+})
+
+mkdirp(outdir, () => {
+  graph.start()
+})
+
+function writeFile (file, data, log) {
+  fs.writeFile(file, data, () => {
+    console.log(log.green)
   })
 }
 
-function writeServiceWorker () {
-  fs.readFile('sw.js', (err, data) => {
-    var filename = path.join(outdir, 'sw.js')
-
-    fs.writeFile(filename, data, () => {
-      console.log('create sw.js'.green)
-    })
-  })
+function createCleanCssOptions () {
+  return {
+    level: {
+      1: {
+        specialComments: 0
+      }
+    }
+  }
 }
