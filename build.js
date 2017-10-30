35d5b391fd12ed9c77c9a985123234482438a404
zhouhansen <z308114274@gmail.com>
2017-10-30 14:07:08 +0800

    use documentify

--- build.js
+++ build.js
@@ -9,6 +9,10 @@ var Clean = require('clean-css')
 var dedent = require('dedent')
 var minify = require('html-minifier').minify
 var concat = require('concat-stream')
+var documentify = require('documentify')
+var hyperstream = require('hyperstream')
+var pump = require('pump')
+var critical = require('inline-critical-css')
 var graph = require('buffer-graph')()
 
 var entry = 'index.js'
@@ -26,9 +30,9 @@ graph.on('change', (nodeName, edgeName, state) => {
     file = path.join(outdir, 'bundle.js')
     data = state.scripts.bundle.buffer
     log = ' ✨  create bundle.js'
-  } else if (eventName === 'html:bundle') {
+  } else if (eventName === 'documents:bundle') {
     file = path.join(outdir, 'index.html')
-    data = state.html.bundle.buffer
+    data = state.documents.bundle.buffer
     log = ' ✨  create index.html'
   } else if (eventName === 'manifest:bundle') {
     file = path.join(outdir, 'manifest.json')
@@ -73,24 +77,98 @@ graph.node('scripts', (state, createEdge) => {
   }
 })
 
-graph.node('html', (state, createEdge) => {
-  var file = html`
-    <!doctype html>
-    <html lang="en" dir="ltr">
-    <head>
-      <title>choo-scriber</title>
-      <link rel="manifest" href="manifest.json">
-      <link rel="stylesheet" href="bundle.css">
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <div></div>
-      <script src="bundle.js"></script>
-    </body>
-    </html>
-  `
+graph.node('documents', [ 'scripts:bundle', 'manifest:bundle', 'serviceWorker:bundle', 'styles:bundle' ], (state, createEdge) => {
+  var body = '<div></div>'
+  var language = 'en'
+  var html = head(body, language)
+  var d = documentify(entry, html)
+  d.transform(viewportTransform)
+  d.transform(polyfillTransform)
+  d.transform(scriptTransform)
+  d.transform(styleTransform)
+  d.transform(preloadTransform)
+  d.transform(manifestTransform)
+  d.transform(descriptionTransform, { description: "A very cute app" })
+  d.transform(titleTransform, { title: 'choo-scriber' })
+  d.transform(criticalTransform, { css: state.styles.bundle.buffer })
 
-  createEdge('bundle', Buffer.from(String(file)))
+  var source = d.bundle()
+
+  pump(source, concat({ encoding: 'buffer' }, sink))
+
+  function sink (buf) {
+    createEdge('bundle', buf)
+  }
+
+  function head (body, lang) {
+    var dir = 'ltr'
+    return `<!DOCTYPE html><html lang="${lang}" dir="${dir}"><head></head>${body}</html>`
+  }
+
+  function criticalTransform (opts) {
+    return critical(String(opts.css))
+  }
+
+  function titleTransform (opts) {
+    var header = `
+      <title>${opts.title}</title>
+    `.replace(/\n +/g, '')
+    return addToHead(header)
+  }
+
+  function descriptionTransform (opts) {
+    var header = `
+      <meta name="description" content="${opts.description}">
+    `.replace(/\n +/g, '')
+    return addToHead(header)
+  }
+
+  function manifestTransform () {
+    var header = `
+      <link rel="manifest" href="/manifest.json">
+    `.replace(/\n +/g, '')
+    return addToHead(header)
+  }
+
+  function preloadTransform () {
+    var content = ';(function(a){"use strict";var b=function(b,c,d){function e(a){return h.body?a():void setTimeout(function(){e(a)})}function f(){i.addEventListener&&i.removeEventListener("load",f),i.media=d||"all"}var g,h=a.document,i=h.createElement("link");if(c)g=c;else{var j=(h.body||h.getElementsByTagName("head")[0]).childNodes;g=j[j.length-1]}var k=h.styleSheets;i.rel="stylesheet",i.href=b,i.media="only x",e(function(){g.parentNode.insertBefore(i,c?g:g.nextSibling)});var l=function(a){for(var b=i.href,c=k.length;c--;)if(k[c].href===b)return a();setTimeout(function(){l(a)})};return i.addEventListener&&i.addEventListener("load",f),i.onloadcssdefined=l,l(f),i};"undefined"!=typeof exports?exports.loadCSS=b:a.loadCSS=b})("undefined"!=typeof global?global:this);'
+    content += ';(function(a){if(a.loadCSS){var b=loadCSS.relpreload={};if(b.support=function(){try{return a.document.createElement("link").relList.supports("preload")}catch(b){return!1}},b.poly=function(){for(var b=a.document.getElementsByTagName("link"),c=0;c<b.length;c++){var d=b[c];"preload"===d.rel&&"style"===d.getAttribute("as")&&(a.loadCSS(d.href,d,d.getAttribute("media")),d.rel=null)}},!b.support()){b.poly();var c=a.setInterval(b.poly,300);a.addEventListener&&a.addEventListener("load",function(){b.poly(),a.clearInterval(c)}),a.attachEvent&&a.attachEvent("onload",function(){a.clearInterval(c)})}}})(this);'
+
+    var header = `<script nomodule>${content}</script>`
+    return addToHead(header)
+  }
+
+  function scriptTransform () {
+    var header = `<script src="bundle.js" defer></script>`
+    return addToHead(header)
+  }
+
+  function styleTransform (opts) {
+    var header = `<link rel="preload" as="style" href="bundle.css" onload="this.rel='stylesheet'">`
+    return addToHead(header)
+  }
+
+  function viewportTransform () {
+    var header = `
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    `.replace(/\n +/g, '')
+    return addToHead(header)
+  }
+
+  function polyfillTransform () {
+    var link = 'https://cdn.polyfill.io/v2/polyfill.min.js'
+    var header = `<script src="${link}" defer></script>`
+    return addToHead(header)
+  }
+
+  function addToHead (str) {
+    return hyperstream({
+      head: {
+        _appendHtml: str
+      }
+    })
+  }
 })
 
 graph.node('manifest', (state, createEdge) => {
